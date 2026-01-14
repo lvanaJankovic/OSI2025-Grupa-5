51ec4f868975b7158071e62a9243d214ec4f3344
DESKTOP-M9P4VSL\User <cuskicmuhamed@gmail.com>
2026-01-14 23:37:41 +0100

    Dodajem konacan program za administratora

---
+++ administrator/administrator.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "upravljanje-konobari.h"
+#include "upravljanje-meni.h"
+#include "upravljanje-prostor.h"
+
+int main() {
+    int izbor;
+    do {
+        printf("\n=== MENI ZA ADMINISTRATORA ===\n");
+        printf("1. Upravljanje osobljem\n");
+        printf("2. Upravljanje menijem\n");
+        printf("3. Upravljanje prostorima i stolovima\n");
+        printf("0. Izlaz\nIzbor: ");
+        scanf("%d",&izbor);
+
+        switch(izbor){
+            case 1: upravljanjeNalozima(); break;
+            case 2: administratorMeni(); break;
+            case 3: upravljanjeProstorima(); break;
+            case 0: printf("Izlaz iz sistema...\n"); break;
+            default: printf("Nepostojeca opcija!\n");
+        }
+
+    } while(izbor!=0);
+
+    return 0;
+}
